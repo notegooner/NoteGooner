ece2e1f514ab89da91c4c434da32bcc0f728272d
notegooner <notegooner@gmail.com>
2022-06-24 12:05:56 +0700

    BeforeLunch

--- lib/states/detail.dart
+++ lib/states/detail.dart
@@ -1,12 +1,16 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'dart:io';
+import 'dart:math';
 
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:image_picker/image_picker.dart';
 
 import 'package:notegooner/models/job_model.dart';
 import 'package:notegooner/utility/my_constant.dart';
 import 'package:notegooner/utility/my_dialog.dart';
+import 'package:notegooner/widgets/show_button.dart';
 import 'package:notegooner/widgets/show_icon_button.dart';
 import 'package:notegooner/widgets/show_image.dart';
 import 'package:notegooner/widgets/show_text.dart';
@@ -64,9 +68,48 @@ class _DetailState extends State<Detail> {
           children: [
             newImage(boxConstraints),
             newDetail(boxConstraints),
+            newMap(boxConstraints),
+            buttonUpload(),
           ],
         );
       }),
+    );
+  }
+
+  ShowButtom buttonUpload() {
+    return ShowButtom(
+      label: 'Upload Image',
+      pressFunc: () {
+        if (file == null) {
+          MyDialog(context: context)
+              .normalDialog(title: 'No Image', subtitle: 'Please Take Photo');
+        } else {
+          processUploadImage();
+        }
+      },
+    );
+  }
+
+  Row newMap(BoxConstraints boxConstraints) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Container(
+          width: boxConstraints.maxWidth * 0.8,
+          height: boxConstraints.maxHeight * 0.6,
+          child: GoogleMap(
+            initialCameraPosition: CameraPosition(
+                target: LatLng(
+                  double.parse(jobModel!.lat.trim()),
+                  double.parse(
+                    jobModel!.lng.trim(),
+                  ),
+                ),
+                zoom: 16),
+            onMapCreated: (value) {},
+          ),
+        ),
+      ],
     );
   }
 
@@ -129,5 +172,18 @@ class _DetailState extends State<Detail> {
             )),
       ],
     );
+  }
+
+  Future<void> processUploadImage() async {
+    String path = 'https://www.androidthai.in.th/egat/saveFileNote.php';
+    String nameFile = 'image${Random().nextInt(1000000)}.jpg';
+    Map<String, dynamic> map = {};
+    map['file'] = await MultipartFile.fromFile(file!.path, filename: nameFile);
+    FormData data = FormData.fromMap(map);
+    await Dio().post(path, data: data).then((value) {
+      String urlImage = 'https://www.androidthai.in.th/egat/noteimage/$nameFile';
+
+      print('Upload Success');
+    });
   }
 }
